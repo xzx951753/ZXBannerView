a37ae4b02ee3ed89d9100efe52412af26838183c
Xie zexin <285644797@qq.com>
2018-03-27 20:59:29 +0800

    0.1.4

---
+++ ZXBannerView-0.1.3/ios/ZXBannerView.framework/Versions/A/Headers/ZXBannerData.h
@@ -0,0 +1,18 @@
+//
+//  ZXBannerData.h
+//  ZXBannerView
+//
+//  Created by Xie zexin on 03/15/2018.
+//  Copyright (c) 2018 Xie zexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZXBannerData : NSObject
+
+@property (nonatomic,copy) NSURL* url;
+@property (nonatomic,copy) NSURL* link;
+
+
+
+@end
--- ZXBannerView.podspec
+++ ZXBannerView.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ZXBannerView'
-  s.version          = '0.1.3'
+  s.version          = '0.1.4'
   s.summary          = 'A simple cycle banner view by OC, depends Masonry and SDWebimage,support touch block.'
 
 # This description is used to generate tags and improve search results.
